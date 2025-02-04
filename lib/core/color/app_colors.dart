d689930e5fdbd057b0799a3d2a0dde5c3d482aa0
iamtherjahid <jahid.info97@gmail.com>
2025-02-04 23:21:01 +0600

    feat (chore): - Primary color changed Predict screen app bar changed

--- lib/core/color/app_colors.dart
+++ lib/core/color/app_colors.dart
@@ -3,6 +3,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xff6ADC01);
+  static const Color primary = Color.fromARGB(255, 94, 172, 21);
   static const Color secondary = Color(0xffDFF2CE);
 }

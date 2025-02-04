7091c42d529f444b27a26b7aaae19ed3128bc392
Jahidul Islam <jahid.info97@gmail.com>
2025-02-04 16:32:29 +0600

    feat (splash) splash screen ui done

--- lib/features/splashscreen/view/splashscreen.dart
+++ lib/features/splashscreen/view/splashscreen.dart
@@ -1,19 +1,53 @@
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:greenshield_ai/core/helpers/ui_exporter.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends HookConsumerWidget {
   static String get name => '/SplashScreen';
   static String get path => '/SplashScreen';
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    init() async {
+      await Future.delayed(2.seconds).then((v) {});
+    }
+
+    useEffect(() {
+      Future.microtask(() => init());
+      return null;
+    }, []);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Title'),
-        centerTitle: true,
-      ),
       body: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+                  child: Image.asset(
+            'assets/green_leaf.png',
+            height: 120.w,
+            width: 120.w,
+          ))
+              .animate()
+              .scale(
+                duration: 1.seconds,
+                curve: Curves.easeInOut,
+              )
+              .fadeIn(duration: 1.seconds),
+          Gap(36),
+          Text(
+            "Welcome to GreenShield AI!",
+            style: GoogleFonts.nunito(
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w800,
+              color: AppColors.primary,
+            ),
+          )
+              .animate()
+              .fadeIn(delay: 500.ms, duration: 1.seconds)
+              .slideY(begin: 1, end: 0, curve: Curves.easeOut),
+        ],
       ),
     );
   }
